b04f33f2218d06e1a9aca73e5ff42bda6f401b93
sarahalpay <sarah.g.alpay@gmail.com>
2018-02-03 17:28:43 +0000

    cryptoCurrency dropdown created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://12b557b7.ngrok.io/coins/',
+      allTheCoins: [],
+      selectedCoin: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData(this.state.url);
+  }
+
+  fetchData(url) {
+    fetch(url)
+      .then(data => data.json())
+      .then(json => this.setState({allTheCoins: json}));
+  }
+
+  fetchSingleCoinData(coin){
+    const url = this.state.url + coin;
+    fetch(url)
+      .then(data => data.json())
+      .then(json => this.setState({selectedCoin: json}));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div>
+        <h1>React Crypto Currency Stock</h1>
+        {this.state.allTheCoins &&
+          <select onChange={(e) => this.fetchSingleCoinData(e.target.value)}>
+            {
+              this.state.allTheCoins.map(coin =>
+                <option key={coin.id} value={coin.id}>{coin.name}</option>
+              )
+            }
+          </select>
+        }
       </div>
     );
   }
